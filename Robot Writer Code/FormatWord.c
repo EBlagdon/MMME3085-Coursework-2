e81297c5c5cbe70b0f7ca70acbb5b0dda79eeced
Ethan Blagdon <egyeb12@nottingham.ac.uk>
2025-12-04 14:22:42 +0000

    tidied comments

--- Robot Writer Code/FormatWord.c
+++ Robot Writer Code/FormatWord.c
@@ -8,29 +8,13 @@ int ReadWord( FILE *WordFile, char (*WordContents)[], int *startposition)
     char currChar;
 	fseek(WordFile, *startposition, SEEK_SET); //sets file position to startposition
         int i=0;
-        currChar = fgetc(WordFile);
+        currChar = fgetc(WordFile); //get first character
         while (i < MAXCN && currChar != ' ' && currChar != '\n' && currChar != EOF) { //Read the word until space, newline, or EOF
             (*WordContents)[i++] = (char)currChar; //saves letter to word contents 
-            currChar = fgetc(WordFile);
+            currChar = fgetc(WordFile); //get next character
         }
         *startposition = ftell(WordFile);        //Update startposition to current file position
         
     return 0;
 }
 
-/*
-int SeperateWords( FILE *WordFile, char (*WordContents)[], int *startposition)
-{
-    char currChar;
-	fseek(WordFile, *startposition, SEEK_SET); //sets file position to startposition
-        int j = *startposition;
-        int i=0;
-        while (j + 1 < MAXCN && ((currChar = fgetc (WordFile)) != ' ' && currChar != EOF)){  //Reads untill the next space
-            (*WordContents)[i++] = (char)currChar; //saves letter to array
-            j++;
-            }
-       *startposition = j + 1; //saves the start position of the next word next time the array is called
-
-    return 0;
-}
-*/
